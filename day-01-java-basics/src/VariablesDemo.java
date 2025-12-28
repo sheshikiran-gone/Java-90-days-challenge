1001e89db4127b2d4d095c48199fb1312c199191
sheshikiran <sheshikiran.gone@gmail.com>
2025-12-29 00:20:50 +0530

    this is my first program

---
+++ day-01-java-basics/src/VariablesDemo.java
@@ -0,0 +1,13 @@
+//public class VariablesDemo {
+//}
+public class VariablesDemo {
+    public static void main(String[] args) {
+        int age = 22;
+        String name = "Sheshikiran";
+        double salary = 1.85;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary + " LPA");
+    }
+}
